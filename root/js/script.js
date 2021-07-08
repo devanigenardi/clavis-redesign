75d912c9b39e34820f9440291bb90be353f8b0ed
devani genardi <devani.genardi@wearepop.com>
2021-07-08 11:45:28 -0600

    navigation

--- root/js/script.js
+++ root/js/script.js
@@ -1,32 +1,31 @@
-$(function() {
-    $('.validity').validity({
-        // selector: 'select',
-        // ignore: 'select',
-        messages: {
-            missing: 'Required field*',
-            mismatch: 'invalid value*'
+
+$(document).ready(function(){
+    // gives bottom same height as footer
+    var footerHeight = $('.footer').outerHeight();
+    $('.bottom__spacer').css({"height" : footerHeight+"px"});
+
+    // hamburger menu
+    $(".hamburger").click(function(){
+        $(".hamburger").toggleClass("active");
+        $(".nav-menu").toggleClass("active");
+    });
+
+    //current location
+    var currentLocation = location.href;
+    var menuItems = $(".nav-link");
+    menuItems.each(function( index ) {
+        if(menuItems[index].href === currentLocation) {
+            menuItems[index].className = "nav-active"
         }
-    })
-        .on('submit', function(e) {
-            var $this = $(this),
-                $btn = $this.find('[type="submit"]');
-                $btn.button('loading');
-
-            if (!$this.valid()) {
-                e.preventDefault();
-                $btn.button('reset');
-            }
     });
 
-    $('.validity2').validity()
-        .on('submit', function(e) {
-            var $this = $(this),
-                $btn = $this.find('[type="submit"]');
-                $btn.button('loading');
+});
 
-            if (!$this.valid()) {
-                e.preventDefault();
-                $btn.button('reset');
-            }
-    });
+$(window).scroll(function(){
+    // creates the footer
+    if ($(this).scrollTop() > 1000) {
+        $('.footer').removeClass('hide-element');
+    } else {
+        $('.footer').addClass('hide-element');
+    }
 });
