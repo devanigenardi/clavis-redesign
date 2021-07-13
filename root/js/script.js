bd3e5ce4f6c58f8f83f79b816b11ab3cb4460e18
devani genardi <devani.genardi@wearepop.com>
2021-07-13 16:58:34 -0600

    what we believe section

--- root/js/script.js
+++ root/js/script.js
@@ -74,25 +74,6 @@ $(document).ready(function(){
       });
       
 
-      // paralax
-
-        var initScrollTop = $(window).scrollTop();
-
-        // Set the image's vertical background position based on the scroll top when the page is loaded.
-        $(parallax1).css({'background-position-y' : (initScrollTop/5)+'%'});
-
-        // When the user scrolls...
-        $(window).scroll(function() {
-
-        // Find the new scroll top.
-        var scrollTop = $(window).scrollTop();
-
-        // Set the new background position.
-        $(parallax1).css({'background-position-y' : (scrollTop/5)+'%'});
-
-        });
-      
-
 });
 
 // $(window).scroll(function(){
